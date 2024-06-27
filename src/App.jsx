28fb74ab6579d3b1528f5f4784dcd9619a5c6c8e
mimi110 <miminguyen173@gmail.com>
2024-06-26 22:16:18 -0500

    Create Event Management Form

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Signup from './Authentication/Signup';
 import Login from './Authentication/Login';
+import EventForm from './Event_Form/Event_Form';
 import UserProfile from './User_Profile/UserProfile.jsx'
 import Notification from './Notification.jsx';
 import './App.css';
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/eventform" element={<EventForm />} />  
           <Route path="/userprofile" element={<UserProfile />} />
           <Route path="/notification" element={<Notification />} />
         </Routes>
